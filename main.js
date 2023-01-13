3265afa2c0e3c234173119835c5a7827f07cdc40
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-01-13 19:35:08 +0530

    HTML & CSS Completed

--- main.js
+++
@@ -1,6 +0,0 @@
-const playerContainer = document.querySelector("[data-player-conatainer]");
-
-// querySelector("[data-play]")
-document.addEventListener("click",()=>{
-    playerContainer.classList.toggle("play");
-})
